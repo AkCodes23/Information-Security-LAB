505d590e3c96c7e9faf38996d8d846085d125321
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2025-08-12 09:52:35 +0530

    Update Q2.py

--- Lab 2/Q2.py
+++ Lab 2/Q2.py
@@ -1,19 +1,19 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
-# Message and key
+
 message = b"Sensitive Information"
 key = bytes.fromhex("0123456789ABCDEF0123456789ABCDEF")  # 16-byte key
 
-# Create AES cipher in ECB mode
+
 cipher = AES.new(key, AES.MODE_ECB)
 
-# Encrypt
+
 padded_text = pad(message, AES.block_size)
 ciphertext = cipher.encrypt(padded_text)
 print("Encrypted:", ciphertext.hex())
 
-# Decrypt
 decrypted_padded = cipher.decrypt(ciphertext)
 decrypted = unpad(decrypted_padded, AES.block_size)
 print("Decrypted:", decrypted.decode())
+
