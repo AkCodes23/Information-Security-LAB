c7c75b499987bce24e2f3c70ca32eed6bfb3dc62
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2025-08-12 09:53:25 +0530

    Update Q5.py

--- Lab 2/Q5.py
+++ Lab 2/Q5.py
@@ -1,18 +1,17 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
-# Message and key
-message = b"Top Secret Data"
-key = bytes.fromhex("FEDCBA9876543210FEDCBA9876543210")  # 24 bytes for AES-192
 
-# Create AES cipher
+message = b"Top Secret Data"
+key = bytes.fromhex("FEDCBA9876543210FEDCBA9876543210") 
+
+
 cipher = AES.new(key, AES.MODE_ECB)
 
-# Encrypt
 padded = pad(message, AES.block_size)
 ciphertext = cipher.encrypt(padded)
 print("Encrypted:", ciphertext.hex())
 
-# Decrypt
 decrypted = unpad(cipher.decrypt(ciphertext), AES.block_size)
 print("Decrypted:", decrypted.decode())
+
